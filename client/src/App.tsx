8c74c1957d6fbf474e100e14cc6d259ca800ad2f
evgenii-sorokin-percent <eugenesorokin_301@outlook.com>
2026-09-01 19:23:18 +0300

    feat: add auth page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import NotFound from "./pages/NotFound/NotFound";
 import DefaultLayout from "./layouts/DefaultLayout/Default";
+import Auth from "./pages/Auth/Auth";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DefaultLayout />}>
+          <Route path="/auth" element={<Auth />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
--- client/src/pages/Auth/Auth.tsx
+++ client/src/pages/Auth/Auth.tsx
@@ -1,3 +1,20 @@
-const Auth = () => {};
+import styles from "./Auth.module.scss";
+
+import { UserRound, LogIn } from "lucide-react";
+
+const Auth = () => {
+  return (
+    <div className={`container flex-center ${styles.auth}`}>
+      <article className={`${styles["auth__card"]}`}>
+        <UserRound size={128} />
+        <h2>Войти без аккаунта</h2>
+      </article>
+      <article className={`${styles["auth__card"]}`}>
+        <LogIn size={128} />
+        <h2>Войти в аккаунт</h2>
+      </article>
+    </div>
+  );
+};
 
 export default Auth;
